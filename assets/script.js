793c6b0321e7d9883e1bcfd350c5e4b49df5a3ad
cfprice3 <cfprice3@gmail.com>
2020-02-27 00:04:27 -0500

    creating variables for color change

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,9 @@ $(document).ready(function () {
     $("#currentDay").text(moment().format('dddd') + ", " + moment().format('LL'));
     writeToSchedule();
 });
+
+// Variables that will dictate what bg color each hour is.
+var time = moment();
+var format = 'hh:mma';
+var topOf9 = moment('9:00am', format);
+var bottomOf9 = moment('10:00am', format);
